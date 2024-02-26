1b214cd0ae06092eb6d45f4bc34856e613e5fbc7
3shaan <3shaan.dev@proton.me>
2024-02-26 21:30:53 +0600

    fish add

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+set -g fish_greeting
+
+if status is-interactive
+    #    starship init fish | source
+end
+
+# List Directory
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
+
+# Fixes "Error opening terminal: xterm-kitty" when using the default kitty term to open some programs through ssh
+alias ssh='kitten ssh'
+
+## keybinding 
+
+bind --preset \ch backward-char # goes forword
+bind --preset \ck forward-char # goes backeword
+bind --preset \cd backward-delete-char #clear backword 
